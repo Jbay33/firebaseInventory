140680b83247821a3490143eacadd583d91044e7
Justin Baylis <jlukebay@gmail.com>
2023-10-11 13:45:12 -0400

    Initial Commit

---
+++ firebaseInventory/firebaseInventoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  firebaseInventoryApp.swift
+//  firebaseInventory
+//
+//  Created by  on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct firebaseInventoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
